3ee0116bacc13063f824af857e0b2c99e78050fb
RingiBoy <mitya.info@gmail.com>
2019-12-26 22:35:58 +0200

    новый файл

--- namber.py
+++ namber.py
@@ -1,41 +1,64 @@
 import random
-import math
 
-#
-PC_rendom = random.randint(1, 50)
-# modul=0
-# tryID = 0
-kol_raz = 0
-#
-while kol_raz < 6:
-    user_variant = input()
-##
-    modul = math.fabs(int(user_variant) - PC_rendom)
-    # print(f'число:  {PC_rendom} ')
-    if int(user_variant) == PC_rendom:
-        print('УГАДАЛ!!!!!УРААААА!!!')
-        break
-    else:
-        kol_raz = kol_raz + 1
-        altog = 6 - kol_raz
+################Имена кто первый начинает? #######################
+name1 = input('Первое имя игрока: ')
+name2 = input('Второе имя игрока: ')
+players = [name1, name2]
+pal = int(input('ввод количества палочек: '))  # количество палок, стартовый ввод значения
+print(f'Итак, игра началась, выбрано: {pal} палочек!')
+choice_Who = random.choice(players)
+if choice_Who == name1:
+    print(f"Первый вытягивает {name1}")
+    turn_players = [name1, name2]
+else:
+    print(f"Первый вытягивает {name2}")
+    turn_players = [name2, name1]
 
-        if altog < 1:
-            print(f'жаль конечно, вы были так близки, правильное число: {PC_rendom}')
+AllRule=0
+##############################################################
+# Вводные данные:
+
+while pal > 1:
+    #Игроки по очереди тянут палки
+    Rule1_3 = 1
+    for name in turn_players:
+
+        while Rule1_3 == 1:
+            input_number_player = int(input('сколько палочек вытянуть:'))
+            if pal >= 4:
+                if input_number_player < 1 or input_number_player > 3:
+                    print("Введите правильное значение от 1-3")
+                    Rule1_3 = 1
+                elif input_number_player >= 1 and input_number_player <= 3:
+                    print('правило 1-3 выполнено')
+                    Rule1_3 = 0  # ввод заканчивается и выходим из цикла коректировки 1-3
+            if pal == 3:
+                if input_number_player == 3:
+                    print("Введите допустимое значение от 1-2")
+                    Rule1_3 = 1
+                elif input_number_player >= 1 and input_number_player <= 2:
+                        Rule1_3 = 0
+            if pal == 2:
+                if input_number_player == 2:
+                    print("вы можете вытянуть только 1 палочку")
+                    Rule1_3 = 1
+                elif input_number_player == 1:
+                    print('правило 1-1 выполнено')
+                    Rule1_3 = 0  # ввод заканчивается и выходим из цикла коректировки 1-1
+                    print(f"Віиграл {name}")
+                    break
+            if pal == 1:  # на всякий случай
+                print('проиграл игрок:')
+                print(name)
+                break
+        pal = pal - input_number_player
+        if pal <= 1:
+            print('выиграл')
+            print(name)
             break
-        elif modul >= 20:
-            print(f'очень холодно 20, еще остались {altog} попытok')
+
+        else:
+            print(f'остаток:{pal}')
+            print(f'тянул:{name}')
+            Rule1_3=1
             continue
-        elif modul >= 10:
-            print(f'холодно 10, еще остались {altog} попытok')
-            continue
-        elif modul >= 5:
-            print(f'тепло 5, еще остались {altog} попытok')
-            continue
-        elif modul >= 2:
-            print(f'горячо 2, еще остались {altog} попытok')
-            continue
-        elif modul == 1:
-            print(f'очень очень!!!!!! +-1, еще остались {altog} попытok')
-            continue
-        print(f' еще остались {altog} попытok')
-###
